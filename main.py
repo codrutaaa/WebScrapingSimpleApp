1b74c7558c6b1e7cb5106e8d0a0e5eaf8c040f9b
CodrutaAlexandra <codruta.lacatus02@e-uvt.ro>
2024-10-12 13:03:48 +0300

    web scraping the Hacker News site

---
+++ main.py
@@ -0,0 +1,30 @@
+import requests
+# allows us to use the html and grab different data to scrape it
+from bs4 import BeautifulSoup
+# for a cleaner printed data
+import pprint
+
+res = requests.get('https://news.ycombinator.com/news') # url that we want to grab the data from
+soup = BeautifulSoup(res.text, 'html.parser') # ac e un html si vreau sa il parsezi / exsta mai multe parsari lxml/html
+links = soup.select('.titleline')
+subtext = soup.select('.subtext')
+
+# vrem sa luam doar
+def create_custom_hn(links, subtext):
+
+    hn = [] #empty list
+    for idx, item in enumerate(links): # enum links for using both lists links&subtext
+        title = links[idx].getText()   # title is equal with name of each site
+        href= links[idx].get('href', None) # grab the links too
+        vote = subtext[idx].select('.score')
+        if len(vote):
+            points = int(vote[0].getText().replace('points',' ')) # grab the votes
+            # were problems because sometimes the links doesn't have votes
+            if points > 99 :
+                hn.append({'title':title, 'link':href, 'votes':points})
+    return hn
+
+pprint.pprint(create_custom_hn(links, subtext))
+
+
+
